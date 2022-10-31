fb3e1e9f62a13d25dc644fe845da9f8b32b81ec2
xphere <perefm@gmail.com>
2022-10-31 10:14:09 +0100

    Fixed compiling issues by linking as static library

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -3,6 +3,6 @@ git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 powershell "dir *.ps* -recurse | Set-AuthenticodeSignature -Certificate (dir cert:\currentuser\my -code)"
 call bootstrap-vcpkg.bat
-rem vcpkg install --recurse physfs --triplet x64-windows-static
-vcpkg install --recurse physfs
+vcpkg install --recurse physfs --triplet x64-windows-static
+rem vcpkg install --recurse physfs
 cd..
--- 02_run_cmake.bat
+++ 02_run_cmake.bat
@@ -1,8 +1,8 @@
 rmdir physfs_test_vs2022 /s /q
 mkdir physfs_test_vs2022
 cd physfs_test_vs2022
-rem cmake.exe -DVCPKG_TARGET_TRIPLET=x64-windows-static -G "Visual Studio 17 2022" ..
-cmake.exe -DBUILD_SHARED_LIBS=ON -DVCPKG_TARGET_TRIPLET=x64-windows -G "Visual Studio 17 2022" ..
+cmake.exe -DVCPKG_TARGET_TRIPLET=x64-windows-static -G "Visual Studio 17 2022" ..
+rem cmake.exe -DBUILD_SHARED_LIBS=ON -DVCPKG_TARGET_TRIPLET=x64-windows -G "Visual Studio 17 2022" ..
 cd..
 
 pause
