b43322908960e0b098fcbbafcb5deaf678c5e7fc
xphere <perefm@gmail.com>
2022-10-31 11:31:31 +0100

    added some more code to read files

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,42 @@
 #include "main.h"
 
 
+
+char* readFile(std::string& fileName, int& fileSize)
+{
+	PHYSFS_file* file = PHYSFS_openRead(fileName.c_str());
+	if (file == NULL) {
+		printf("Error opening file %s, error: %s\n", fileName.c_str(), PHYSFS_getLastError());
+		return NULL;
+	}
+
+	fileSize = static_cast<int>(PHYSFS_fileLength(file));
+	printf("test.txt size: %d\n", fileSize);
+
+	char* buffer = new char[fileSize];
+	PHYSFS_read(file, buffer, 1, static_cast<PHYSFS_uint32>(fileSize));
+	PHYSFS_close(file);
+
+	return buffer;
+	/*#elif defined(EMSCRIPTEN_TARGET)
+		std::ifstream file(_filename, std::ios::in|std::ios::binary|std::ios::ate);
+		assert(file.is_open());
+
+		*_size = file.tellg();
+		assert(*_size != 0);
+
+		natU8* buffer = new natU8[*_size];
+		file.seekg (0, std::ios::beg);
+		file.read ((char*)buffer, *_size);
+		file.close();
+
+		return buffer;
+	#else
+
+	#endif*/
+}
+
+
 int main() {
 
 	float time = 0;
@@ -23,6 +59,23 @@ int main() {
 
 	if (0 == PHYSFS_mount("files//data.zip", NULL, 0))
 		printf("Error opening file, error: %s\n", PHYSFS_getLastError());
+
+	/*
+	PHYSFS_file* file = PHYSFS_openRead("test.txt");
+	if (!file) {
+		printf("Error opening file inside zip, error: %s\n", PHYSFS_getLastError());
+		return 0;
+	}
+		
+	PHYSFS_sint64 file_size = PHYSFS_fileLength(file);
+	printf("test.txt size: %d\n", (unsigned int)file_size);
+	*/
+	int file_size = 0;
+	std::string file = "test.txt";
+	char* buffer = readFile(file, file_size);
+
+	printf("File content: %s\n", buffer);
+
 
 	while (true)
 	{
