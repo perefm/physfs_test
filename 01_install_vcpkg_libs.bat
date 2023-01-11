00a8e94aea553345a3a9b36c180fb02c1e7f3c80
perefm <perefm@gmail.com>
2023-01-11 16:25:40 +0100

    minor update on vcpkg package management

--- 01_install_vcpkg_libs.bat
+++ 01_install_vcpkg_libs.bat
@@ -3,5 +3,5 @@ git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 powershell "dir *.ps* -recurse | Set-AuthenticodeSignature -Certificate (dir cert:\currentuser\my -code)"
 call bootstrap-vcpkg.bat
-vcpkg install --recurse physfs stb --triplet x64-windows
+vcpkg install --recurse physfs stb --triplet x64-windows --keep-going --clean-after-build
 cd..
